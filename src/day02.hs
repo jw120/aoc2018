bb24b3a583b26b3b1127d4557884ef32a3094193
Joe Watson <joe_watson@mail.com>
2018-12-03 21:38:11 +0800

    Solved day02

--- src/day02.hs
+++ src/day02.hs
@@ -31,13 +31,47 @@ buildCountMap :: Ord k => [k] -> Map k Int
 buildCountMap = foldl' addCount Map.empty
     where addCount m x = Map.insertWith (+) x 1 m
 
+-- Part (b) solution. Finds pair of ids which have only one differing character and
+-- return the common characters
+pairWithOneDiff :: [String] -> String
+pairWithOneDiff ids = commonElements
+    where
+      commonElements = map (\(a, _) -> a) . filter (\(a, b) -> a == b) $ zip id1 id2
+      (True, id1, id2) = head $ filter (\(d, _, _) -> d) labelledPairs
+      labelledPairs = map (\(id1, id2) -> (hasOneDiff id1 id2, id1, id2)) pairs
+      pairs = allPairs ids
 
--- Part (b) solution
+-- | Generate all unordered pairs of elements from a list
+--
+-- >>> allPairs "abcd"
+-- [('a','b'),('a','c'),('a','d'),('b','c'),('b','d'),('c','d')]
+allPairs :: [a] -> [(a, a)]
+allPairs (x: xs) = zip (repeat x) xs ++ allPairs xs
+allPairs [] = []
 
+-- | Are the two lists identical except for precisely one difference
+--
+-- >>> hasOneDiff "abcd" "abxd"
+-- True
+-- >>> hasOneDiff "xyz" "xyz"
+-- False
+-- >>> hasOneDiff "pqrs" "pQrS"
+-- False
+-- >>> hasOneDiff "abc" "abcd"
+-- False
+hasOneDiff :: Eq a => [a] -> [a] -> Bool
+hasOneDiff = hasOneDiff' False
+  where
+    hasOneDiff' found (x:xs) (y:ys)
+      | x == y = hasOneDiff' found xs ys -- Keep going if not different
+      | found = False -- Fail if find second difference
+      | otherwise = hasOneDiff' True xs ys -- Keep going with first difference
+    hasOneDiff' found [] [] = found -- Succed if only one difference and both at end
+    hasOneDiff' _ _ _ = False -- Fail if mismatched lists
 
 main :: IO ()
 main = do
   input <- readFile "input/day02.txt"
   let ids = lines input
   putStrLn $ "day 02 part a: " ++ show (checkSum ids)
-  putStrLn $ "day 02 part b: NYI"
+  putStrLn $ "day 02 part b: " ++ pairWithOneDiff ids
