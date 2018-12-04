b8bf4cfb0d68b082ae2fcb638bd800345ca695a4
Joe Watson <joe_watson@mail.com>
2018-12-04 21:50:10 +0800

    Added claim parsing

--- src/day03.hs
+++ src/day03.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 
 module Day03 where
+
+import Data.Attoparsec.ByteString.Char8 (parseOnly, Parser, char, decimal, skipSpace, endOfLine, maybeResult)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import Data.ByteString (ByteString)
 
 data Claim = Claim {
   id :: Int,
@@ -12,14 +17,32 @@ data Claim = Claim {
 
 -- | Parse a Claim from a string
 --
--- >>> parseClaim "#1 @ 1,3: 4x4"
--- undefined
-parseClaim :: String -> Claim
-parseClaim = undefined
+-- >>> parseClaim $ BC.pack "#1 @ 1,3: 4x4"
+-- Claim {id = 1, x = 1, y = 3, w = 4, h = 4}
+parseClaim :: ByteString -> Claim
+parseClaim s = case (parseOnly claimParser s) of
+  Left msg -> error msg
+  Right claim -> claim
+  where
+    claimParser = do
+      char '#'
+      id <- decimal
+      skipSpace
+      char '@'
+      skipSpace
+      x <- decimal
+      char ','
+      y <- decimal
+      char ':'
+      skipSpace
+      w <- decimal
+      char 'x'
+      h <- decimal
+      return $ Claim id x y w h
 
 main :: IO ()
 main = do
-  input <- readFile "input/day03.txt"
-  let claims = map parseClaim $ lines input
+  input <- B.readFile "input/day03.txt"
+  let claims = map parseClaim $ BC.lines input
   putStrLn $ "day 03 part a: NYI"
   putStrLn $ "day 03 part b: NYI"
