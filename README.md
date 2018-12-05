930dd28391deb8b8e33d5f3bec0a51f3c7ed418a
Joe Watson <joe_watson@mail.com>
2018-12-05 11:59:18 +0800

    Solved 3a (but slow)

--- README.md
+++ README.md
@@ -4,7 +4,23 @@ Programming exercises from adventofcode.com
 
 ## TODO
 
+## Notes
+
+Time taken for day03a
+
+Using Data.Array 60s
+
 ## Tips to build
+
+Normal commands
+
+To compile: stack build
+To compile and run: stack run day01
+To run: stack exec day01
+To run doctests: stack test
+
+
+To set up
 
 stack build intero
 stack install doctest
--- src/day03.hs
+++ src/day03.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
-
 module Day03 where
 
+import Data.Array (Array, listArray, (!), (//))
 import Data.Attoparsec.ByteString.Char8 (parseOnly, Parser, char, decimal, skipSpace, endOfLine, maybeResult)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import Data.ByteString (ByteString)
+import Data.Foldable (foldl')
+
+fabricSize :: Int
+fabricSize = 1000
 
 data Claim = Claim {
   id :: Int,
@@ -40,9 +43,21 @@ parseClaim s = case (parseOnly claimParser s) of
       h <- decimal
       return $ Claim id x y w h
 
+-- Main function for part (a) - number of overlapping squares within the claims
+overlaps :: [Claim] -> Int
+overlaps claims = foldl' countOverlap 0 fullFabric
+  where
+    emptyFabric = listArray ((0, 0), (fabricSize - 1, fabricSize - 1)) (repeat 0)
+    fullFabric = foldl' addClaim emptyFabric claims
+    addClaim :: Array (Int, Int) Int -> Claim -> Array (Int, Int) Int
+    addClaim a (Claim _ x y w h) =
+      a // [((i, j), a ! (i, j) + 1) | i <- [x .. x + w - 1], j <- [y .. y + h - 1]]
+    countOverlap :: Int -> Int -> Int
+    countOverlap acc x = acc + if x > 1 then 1 else 0
+
 main :: IO ()
 main = do
   input <- B.readFile "input/day03.txt"
   let claims = map parseClaim $ BC.lines input
-  putStrLn $ "day 03 part a: NYI"
+  putStrLn $ "day 03 part a: " ++ show (overlaps claims)
   putStrLn $ "day 03 part b: NYI"
