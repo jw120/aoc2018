59208de73663dfa909d91d5456bcd00beec7c7b6
Joe Watson <joe_watson@mail.com>
2018-12-08 23:28:17 +0800

    Day 4a as far as max minute

--- src/day04.hs
+++ src/day04.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 module Day04 where
 
@@ -7,6 +7,9 @@ import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import Data.ByteString (ByteString)
+import Data.List (foldl', sortBy)
+import qualified Data.Map as M
+import Data.Map (Map)
 
 type Guard = Int
 type Timestamp = Int -- in range 0..59
@@ -20,6 +23,16 @@ data ObsAction
   | InPeriodGuardChange Guard Timestamp -- new guard
   | EndDayGuardChange Guard -- new guard at end of the day (23:xx)
   deriving (Show)
+
+hour :: ObsAction -> Int
+hour (EndDayGuardChange _) = 23
+hour _ = 0
+
+minute :: ObsAction -> Int
+minute (EndDayGuardChange _) = error "No minute kept for end-day guard change"
+minute (Wake t) = t
+minute (Sleep t) = t
+minute (InPeriodGuardChange _ t) = t
 
 -- Records of observations
 data ObsRecord = ObsRecord Month Day ObsAction deriving (Show)
@@ -65,14 +78,113 @@ parseObs s = case (A.parseOnly obsParser s) of
               else return (ObsRecord month day (EndDayGuardChange guard))
         ]
 
+cmpObs :: ObsRecord -> ObsRecord -> Ordering
+cmpObs (ObsRecord m1 d1 a1) (ObsRecord m2 d2 a2) =
+  case (compare m1 m2) of
+    GT -> GT
+    LT -> LT
+    EQ -> case (compare d1 d2) of
+      GT -> GT
+      LT -> LT
+      EQ -> case (compare (hour a1) (hour a2)) of
+        GT -> GT
+        LT -> LT
+        EQ -> compare (minute a1) (minute a2)
+
+data DayAction
+  = DayGuard Guard
+  | DaySleep Timestamp
+  | DayWake Timestamp
+  deriving (Show)
+
+-- Replace guard changes before midnight with midnight changes the day of the following record
+moveEndDayGuards :: [ObsRecord] -> [ObsRecord]
+moveEndDayGuards (r1@(ObsRecord _ _ (EndDayGuardChange g)) : r2@(ObsRecord m2 d2 _) : rest) =
+  (ObsRecord m2 d2 (InPeriodGuardChange g 0)) : moveEndDayGuards (r2: rest)
+moveEndDayGuards (r1 : r2 : rest) = r1 : moveEndDayGuards (r2: rest)
+moveEndDayGuards x = x
+
+groupByDays :: [ObsRecord] -> [[DayAction]]
+groupByDays [] = []
+groupByDays observations@(first : rest) = (map toDayAction (first : otherFirstDayObs)) :  groupByDays otherDays
+  where
+    (otherFirstDayObs , otherDays) = span (sameDay first) rest
+    sameDay :: ObsRecord -> ObsRecord -> Bool
+    sameDay (ObsRecord m1 d1 _) (ObsRecord m2 d2 _) = m1 == m2 && d1 == d2
+    toDayAction :: ObsRecord -> DayAction
+    toDayAction (ObsRecord _ _ (Wake t)) = DayWake t
+    toDayAction (ObsRecord _ _ (Sleep t)) = DaySleep t
+    toDayAction (ObsRecord _ _ (InPeriodGuardChange g t)) = DayGuard g
+    toDayAction (ObsRecord _ _ (EndDayGuardChange g)) = error "Not expecting end-day"
+
+toSleepIntervals :: [DayAction] -> [(Guard, Timestamp, Timestamp)]
+toSleepIntervals actions = xs
+    where
+      (_, _ , xs) = foldl' f (Nothing, Nothing, []) actions
+      f (Just g, Just t1, acc) (DayWake t2) = (Just g, Nothing, acc ++ [(g, t1, t2)])
+      f (Just g, Nothing, acc) (DaySleep t) = (Just g, Just t, acc)
+      f (_, _, acc) (DayGuard g) = (Just g, Nothing, acc)
+      f x y = error $ "Unexpected pattern" ++ show (x, y)
+
+toGuardTotals :: [(Guard, Timestamp, Timestamp)] -> Map Guard Timestamp
+toGuardTotals = foldl' f (M.empty)
+    where
+      f :: Map Guard Timestamp -> (Guard, Timestamp, Timestamp) -> Map Guard Timestamp
+      f m (g, t1, t2) = M.insertWith (+) g (t2 - t1) m
+
 -- Main function for part (a)
-sleepiestGuard :: [ObsRecord] -> Int
+sleepiestGuard :: [[DayAction]] -> Int
 sleepiestGuard = undefined
 
+-- Return the key of the map with the highest value (or the first such key if more than one)
+keyOfMaxValue :: Map Guard Timestamp -> Guard
+keyOfMaxValue = fst . M.foldlWithKey' f (-999, 0)
+  where
+    f (maxG, maxT) g t
+      | t > maxT = (g, t)
+      | otherwise = (maxG, maxT)
+
+maxMinute :: [(Timestamp, Timestamp)] -> Timestamp
+maxMinute = undefined
+
+answer :: ByteString -> IO String
+answer input = do
+  putStrLn "sbservations"
+  putStrLn . unlines $ map show observations
+  putStrLn "sortedObservations"
+  putStrLn . unlines $ map show sortedObservations
+  putStrLn "cleanedObservations"
+  putStrLn . unlines $ map show cleanedObservations
+  putStrLn "obsByDays"
+  putStrLn . unlines $ map show obsByDays
+  putStrLn "sleepIntervals"
+  putStrLn . unlines $ map show sleepIntervals
+  putStrLn "guardTotals"
+  putStrLn . unlines $ map show (M.toList guardTotals)
+  putStrLn "sleepiestGuard"
+  putStrLn $ show sleepiestGuard
+  putStrLn "sleepiestGuardIntervals"
+  putStrLn . unlines $ map show sleepiestGuardIntervals
+  putStrLn "sleepiestMinute"
+  putStrLn $ show sleepiestMinute
+  return $ show (sleepiestGuard * sleepiestMinute)
+  where
+    observations :: [ObsRecord] = map parseObs $ BC.lines input
+    sortedObservations :: [ObsRecord] = sortBy cmpObs observations
+    cleanedObservations :: [ObsRecord] = moveEndDayGuards  sortedObservations
+    obsByDays :: [[DayAction]] = groupByDays cleanedObservations
+    sleepIntervals :: [(Guard, Timestamp, Timestamp)] = concatMap toSleepIntervals obsByDays
+    guardTotals :: Map Guard Timestamp = toGuardTotals sleepIntervals
+    sleepiestGuard :: Guard = keyOfMaxValue guardTotals
+    sleepiestGuardIntervals :: [(Timestamp, Timestamp)] =
+      map (\(_, t1, t2) -> (t1, t2)) $
+      filter (\(g, _, _) -> g == sleepiestGuard) sleepIntervals
+    sleepiestMinute :: Timestamp = maxMinute sleepiestGuardIntervals
 
 main :: IO ()
 main = do
-  input <- B.readFile "input/day04.txt"
-  let observations = map parseObs $ BC.lines input
-  putStrLn $ "day 04 part a: " ++ show (sleepiestGuard observations)
+  input <- B.readFile "input/day04_test.txt"
+  a <- answer input
+  putStrLn $ "day 04 part a: " ++ a
   putStrLn $ "day 04 part b: NYI"
+
