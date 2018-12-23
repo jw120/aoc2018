5b4f5cfe023e9879091408c57d190abee940a372
Joe Watson <joe_watson@mail.com>
2018-12-23 23:34:27 +0900

    Solved 13a

--- src/day13.hs
+++ src/day13.hs
@@ -4,6 +4,7 @@ module Day13 where
 
 import Data.Array (Array)
 import qualified Data.Array as A
+import Data.List (foldl', sortBy, tails)
 import Data.Maybe (catMaybes)
 
 --
@@ -64,14 +65,8 @@ instance Show Cart where
 -- Cart's current direction
 data Direction = North | East | South | West deriving Show
 
-move :: Direction -> (Int, Int) -> (Int, Int)
-move North (x, y) = (x, y - 1)
-move South (x, y) = (x, y + 1)
-move East (x, y) = (x + 1, y)
-move West (x, y) = (x + 1, y)
-
 -- Cart's choice at each intersection follows a sequence
-data Choice = TurnLeft | StraightAfterLeft | TurnRight | StraightAfterRight
+data Choice = TurnLeft | GoStraight | TurnRight
 
 instance Show Choice where
   show TurnLeft = "L"
@@ -79,10 +74,9 @@ instance Show Choice where
   show _ = "-"
 
 iterateChoice :: Choice -> Choice
-iterateChoice TurnLeft = StraightAfterLeft
-iterateChoice StraightAfterLeft = TurnRight
-iterateChoice TurnRight = StraightAfterRight
-iterateChoice StraightAfterRight = TurnLeft
+iterateChoice TurnLeft = GoStraight
+iterateChoice GoStraight = TurnRight
+iterateChoice TurnRight = TurnLeft
 
 --
 -- Overall state of system
@@ -106,7 +100,7 @@ testNetworkStr =
 -- | Read the initial state
 --
 -- >>> map show . carts $ readNetwork testNetworkStr
--- ["(2, 0) E-","(9, 3) S-"]
+-- ["(2, 0) EL","(9, 3) SL"]
 -- >>> network $ readNetwork testNetworkStr
 -- /---\
 -- |   |  /----\
@@ -133,7 +127,7 @@ readNetwork xs = State
     unpackCartRow :: (Int, (b, [(Int, Direction)])) -> [(Int, Int, Direction)]
     unpackCartRow (y, (_, xs)) = map (\(x, d) -> (x, y, d)) xs
     mkCart :: (Int, Int, Direction) -> Cart
-    mkCart (x, y, d) = Cart { x = x, y = y, heading = d, next = StraightAfterRight }
+    mkCart (x, y, d) = Cart { x = x, y = y, heading = d, next = TurnLeft }
 
 -- | Read one row
 --
@@ -169,13 +163,107 @@ readRow s = (zip [0..] (map readTrack s), cartIndices s)
     readCart (i, '^') = Just (i, North)
     readCart _ = Nothing
 
+-- | Run system until collision, return collision coordinates
+--
+-- >>> runToCollision $ readNetwork testNetworkStr
+-- (7,3)
+runToCollision :: State -> (Int, Int)
+runToCollision s = case tick s of
+  Left pos -> pos
+  Right s' -> runToCollision s'
+
+runLog :: State -> IO ()
+runLog state = do
+  showState 0 state
+  go 0 state
+    where
+      go i s = case tick s of
+        Left pos -> print pos
+        Right s' -> do
+          showState (i + 1) s'
+          go (i + 1) s'
+      showState i t = putStrLn $ show i ++ ": " ++ show (map (\c -> (x c, y c)) (sortCarts (carts t)))
+
 -- Run system new state or coordinate of first collision
 tick :: State -> Either (Int, Int) State
-tick = undefined
+tick s = fmap (\cs -> s { carts = cs }) newCarts
+  where
+    n :: Network = network s
+    sortedCarts :: [Cart] = sortCarts $ carts s
+    newCarts :: Either (Int, Int) [Cart] = foldl' f (Right []) (tails sortedCarts)
+    f :: Either (Int, Int) [Cart] -> [Cart] -> Either (Int, Int) [Cart]
+    f (Left pos) _ = Left pos
+    f (Right movedCarts) [] = Right movedCarts
+    f (Right movedCarts) (c : unmovedCarts)
+      | collides c' (movedCarts ++ unmovedCarts) = Left (x c', y c')
+      | otherwise = Right (c' : movedCarts)
+      where
+        c' = updateCart n c
+
+sortCarts :: [Cart] -> [Cart]
+sortCarts = sortBy cmpCarts
+  where
+    cmpCarts :: Cart -> Cart -> Ordering
+    cmpCarts c1 c2 = case compare (y c1) (y c2) of
+      LT -> LT
+      GT -> GT
+      EQ -> compare (x c1) (x c2)
+
+-- Does the cart have same (x, y) as any of the carts in the list
+collides :: Cart -> [Cart] -> Bool
+collides c cs = any (sameCoord c) cs
+  where sameCoord c1 c2 = x c1 == x c2 && y c1 == y c2
+
+-- | Update position of cart with one move
+--
+-- >>> take 8 $ iterate (updateCart (network (readNetwork testNetworkStr))) (Cart { x = 2, y = 0, heading = East, next = TurnLeft })
+-- [(2, 0) EL,(3, 0) EL,(4, 0) EL,(4, 1) SL,(4, 2) SL,(5, 2) E-,(6, 2) E-,(7, 2) E-]
+updateCart :: Network -> Cart -> Cart
+updateCart (Network n) c = case (n A.! (x c, y c), heading c) of
+  -- Horizontal
+  (Horizontal, East) -> c_east
+  (Horizontal, West) -> c_west
+  -- Vertical
+  (Vertical, South) -> c_south
+  (Vertical, North) -> c_north
+  --  LeftDown -- '\'
+  (LeftDown, East) -> c_south
+  (LeftDown, North) -> c_west
+  (LeftDown, West) -> c_north
+  (LeftDown, South) -> c_east
+  --  | RightUp-- '/'
+  (RightUp, East) -> c_north
+  (RightUp, North) -> c_east
+  (RightUp, West) -> c_south
+  (RightUp, South) -> c_west
+  -- Intersection
+  (Intersection, East) -> (case next c of
+    TurnLeft -> c_north
+    TurnRight -> c_south
+    _ -> c_east) { next = iterateChoice (next c) }
+  (Intersection, West) -> (case next c of
+    TurnLeft -> c_south
+    TurnRight -> c_north
+    _ -> c_west) { next = iterateChoice (next c) }
+  (Intersection, North) -> (case next c of
+    TurnLeft -> c_west
+    TurnRight -> c_east
+    _ -> c_north) { next = iterateChoice (next c) }
+  (Intersection, South) -> (case next c of
+    TurnLeft -> c_east
+    TurnRight -> c_west
+    _ -> c_south) { next = iterateChoice (next c) }
+  (s, d) -> error $ "Bad update" ++ show s ++ " " ++ show d
+  where
+    c_north = c { y = y c - 1, heading = North }
+    c_south = c { y = y c + 1, heading = South }
+    c_west = c { x = x c - 1, heading = West }
+    c_east = c { x = x c + 1, heading = East }
 
 main :: IO ()
 main = do
   input <- readFile "input/day13.txt"
   let initialState = readNetwork $ lines input
-  putStrLn $ "day 13 part a: " ++ "NYI"
+  putStrLn $ "day 13 part a: " ++ show (runToCollision initialState)
   putStrLn $ "day 13 part b: " ++ "NYI"
+  -- runLog initialState
