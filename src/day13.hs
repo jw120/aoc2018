dceb95d90c2fc133e3a6e7b4ae6cc4abe4cc3e7a
Joe Watson <joe_watson@mail.com>
2018-12-23 17:03:24 +0900

    cart parsing working

--- src/day13.hs
+++ src/day13.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Day13 where
 
 import Data.Array (Array)
 import qualified Data.Array as A
+import Data.Maybe (catMaybes)
 
 -- Segments of the track
 data Segment
@@ -23,11 +24,11 @@ toChar Intersection = '+'
 toChar Empty = ' '
 
 fromChar :: Char -> Segment
-fromChar ' ' = Horizontal
-fromChar ' ' = Vertical
-fromChar ' ' = LeftDown
-fromChar ' ' = RightUp
-fromChar ' ' = Intersection
+fromChar '-' = Horizontal
+fromChar '|' = Vertical
+fromChar '\\' = LeftDown
+fromChar '/' = RightUp
+fromChar '+' = Intersection
 fromChar ' ' = Empty
 fromChar _ = error "Unknown segment char"
 
@@ -41,9 +42,23 @@ instance Show Network where
       row :: Int -> String
       row j = [ toChar (n A.! (x, j)) | x <- [0..xmax]]
 
+-- Cart's current direction
+data Direction = North | East | South | West deriving Show
+
+move :: Direction -> (Int, Int) -> (Int, Int)
+move North (x, y) = (x, y - 1)
+move South (x, y) = (x, y + 1)
+move East (x, y) = (x + 1, y)
+move West (x, y) = (x + 1, y)
+
 -- Cart's choice at each intersection follows a sequence
 
 data Choice = TurnLeft | StraightAfterLeft | TurnRight | StraightAfterRight
+
+instance Show Choice where
+  show TurnLeft = "L"
+  show TurnRight = "R"
+  show _ = "-"
 
 iterateChoice :: Choice -> Choice
 iterateChoice TurnLeft = StraightAfterLeft
@@ -55,8 +70,11 @@ iterateChoice StraightAfterRight = TurnLeft
 data Cart = Cart
   { x :: Int
   , y :: Int
+  , heading :: Direction
   , next :: Choice
 }
+instance Show Cart where
+  show c = "(" ++ show (x c) ++ ", " ++ show (y c) ++ ") " ++ [head (show (heading c))] ++ show (next c)
 
 -- Overall state of system
 data State = State
@@ -76,28 +94,69 @@ testNetworkStr =
 
 -- | Read the initial state
 --
--- >>> network . readNetwork $ map BC.pack testNetworkStr
+-- >>> map show . carts $ readNetwork testNetworkStr
+-- ["(2, 0) E-","(9, 3) S-"]
+-- >>> network $ readNetwork testNetworkStr
 -- /---\
 -- |   |  /----\
 -- | /-+--+-\  |
 -- | | |  | |  |
 -- \-+-/  \-+--/
 --  \------/
--- >>> carts . readNetwork $ map BC.pack testNetworkStr
--- []
 readNetwork :: [String] -> State
 readNetwork xs = State
-  { network = Network $ A.array ((0, xmax), (0, ymax)) undefined
-  , carts = undefined
+  { network = Network $ A.array ((0, xmax), (0, ymax)) trackData
+  , carts = map mkCart cartData
   }
   where
     xmax = (length xs) - 1
     ymax = (length (head xs)) - 1
-    mapWithinRow :: String -> [(Int, Segment)]
-    mapWithinRow = zip [0..] . map fromChar
-    mapAcrossRows :: [(Int, Segment)] -> [((Int, Int), Segment)]
-    mapAcrossRows = zipWith (\x (y, s) -> ((x, y), s)) [0..]
+    rowData :: [(Int, ([(Int, Segment)], [(Int, Direction)]))]
+    rowData = zip [0..] $ map readRow xs
+    trackData :: [((Int, Int), Segment)]
+    trackData = concatMap unpackTrackRow rowData
+    unpackTrackRow :: (Int, ([(Int, Segment)], a)) -> [((Int, Int), Segment)]
+    unpackTrackRow (y, (xs, _)) = map (\(x, s) -> ((x, y), s)) xs
+    cartData :: [(Int, Int, Direction)]
+    cartData = concatMap unpackCartRow rowData
+    unpackCartRow :: (Int, (b, [(Int, Direction)])) -> [(Int, Int, Direction)]
+    unpackCartRow (y, (_, xs)) = map (\(x, d) -> (x, y, d)) xs
+    mkCart :: (Int, Int, Direction) -> Cart
+    mkCart (x, y, d) = Cart { x = x, y = y, heading = d, next = StraightAfterRight }
 
+-- | Read one row
+--
+-- >>> map fst . fst . readRow $ "|   |  /----\\"
+-- [0,1,2,3,4,5,6,7,8,9,10,11,12]
+-- >>> map (toChar . snd) . fst . readRow $ "|   |  /----\\"
+-- "|   |  /----\\"
+-- >>> snd . readRow $ "|   |  /----\\"
+-- []
+-- >>> map fst . fst . readRow $ "/->-\\        "
+-- [0,1,2,3,4,5,6,7,8,9,10,11,12]
+-- >>> map (toChar . snd) . fst . readRow $ "/->-\\        "
+-- "/---\\        "
+-- >>> snd . readRow $ "/->-\\        "
+-- [(2,East)]
+-- >>> snd . readRow $ "| | |  | v  |"
+-- [(9,South)]
+readRow :: String -> ([(Int, Segment)], [(Int, Direction)])
+readRow s = (zip [0..] (map readTrack s), cartIndices s)
+  where
+    readTrack :: Char -> Segment
+    readTrack '>' = Horizontal
+    readTrack '<' = Horizontal
+    readTrack '^' = Vertical
+    readTrack 'v' = Vertical
+    readTrack c = fromChar c
+    cartIndices :: String -> [(Int, Direction)]
+    cartIndices = catMaybes . map readCart . zip [0..]
+    readCart :: (Int, Char) -> Maybe (Int, Direction)
+    readCart (i, '>') = Just (i, East)
+    readCart (i, 'v') = Just (i, South)
+    readCart (i, '<') = Just (i, West)
+    readCart (i, '^') = Just (i, North)
+    readCart _ = Nothing
 
 main :: IO ()
 main = do
