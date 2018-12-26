bd55f4e76c25bcab403ae12832e5f1327ec0700b
Joe Watson <joe_watson@mail.com>
2018-12-26 21:06:22 +0900

    started move

--- src/day15.hs
+++ src/day15.hs
@@ -2,8 +2,10 @@
 
 module Day15 where
 
+import Control.Monad (when)
 import Data.Array.IArray (Array)
 import qualified Data.Array.IArray as A
+import Data.List (nub, sort)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
@@ -25,24 +27,63 @@ instance Show State where
       row j = [toChar (cavern s A.! (x, j)) ((x, j) `M.lookup` (mobs s)) | x <- [0 .. xmax]]
       toChar :: Bool -> Maybe Mob -> Char
       toChar False _ = '#'
-      toChar True (Just (Mob Elf _)) = 'E'
-      toChar True (Just (Mob Goblin _)) = 'G'
+      toChar True (Just mob) = if side mob == Elf then 'E' else 'G'
       toChar True Nothing = '.'
 
 newtype Coord = Coord (Int, Int) deriving (Eq, Ord, A.Ix)
 
--- Accessor function gives false for coordinates outside the array
-isOpen :: State -> (Int, Int) -> Bool
-isOpen s c
-  | A.inRange (A.bounds (cavern s)) c = cavern s A.! c
-  | otherwise = False
+-- -- Accessor function gives false for coordinates outside the array
+-- isOpen :: State -> (Int, Int) -> Bool
+-- isOpen s c
+--   | A.inRange (A.bounds (cavern s)) c = cavern s A.! c
+--   | otherwise = False
 
-data Side = Elf | Goblin
+data Side = Elf | Goblin deriving (Eq, Show)
 
-data Mob = Mob Side Int
+data Mob = Mob
+  { side :: Side
+  , health :: Int
+  } deriving (Show)
 
 initialHealth :: Int = 200
 
+--
+-- Mob move logic
+--
+-- | Move the mob at given coordinates
+move :: Bool -> State -> (Int, Int) -> IO State
+move log s (x, y) = do
+    when log $ print s
+    let mySide = case M.lookup (x, y) (mobs s) of
+                    Just mob -> side mob
+                    Nothing -> error $ "Cannot find myself at " ++ show (x, y)
+    when log $ putStrLn ("Moving " ++ show mySide ++ " from " ++ show (x, y))
+    let enemyPositions = map fst . M.toList . M.filter ((/= mySide) . side) $ mobs s
+    when log $ putStrLn ("Enemies: " ++ show enemyPositions)
+    let openPositions = nub . filter (isOpen s) $ concatMap adjacent enemyPositions
+    when log $ putStrLn ("Open positions: " ++ show openPositions)
+    return s
+
+-- | Return all adjacent coordinates
+--
+-- >>> sort $ adjacent (2, 1)
+-- [(1,1),(2,0),(2,2),(3,1)]
+adjacent :: (Int, Int) -> [(Int, Int)]
+adjacent (x, y) = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+
+-- | Is the given coordinate open (i.e. not a mob or wall)
+--
+isOpen :: State -> (Int, Int) -> Bool
+isOpen s c = cavern s A.! c && M.notMember c (mobs s)
+
+-- | Read State from input
+--
+-- >>> test1
+-- #######
+-- #.G.E.#
+-- #E.G.E#
+-- #.G.E.#
+-- #######
 readState :: [String] -> State
 readState xs = State
   { cavern =  A.array ((0, 0), (xMax, yMax)) cavernData
@@ -54,18 +95,14 @@ readState xs = State
     cavernData :: [((Int, Int), Bool)] = map (\(c, x) -> (c, x `elem` ".GE")) tiles
     mobsData :: [((Int, Int), Mob)] = mapMaybe toMob tiles
     toMob :: ((Int, Int), Char) -> Maybe ((Int, Int), Mob)
-    toMob (c, 'E') = Just (c, Mob Elf initialHealth)
-    toMob (c, 'G') = Just (c, Mob Goblin initialHealth)
+    toMob (c, 'E') = Just (c, Mob { side = Elf, health = initialHealth })
+    toMob (c, 'G') = Just (c, Mob { side = Goblin, health = initialHealth })
     toMob _ = Nothing
 
--- | Test data
 --
--- >>> test1
--- #######
--- #.G.E.#
--- #E.G.E#
--- #.G.E.#
--- #######
+-- Test Data
+--
+
 test1 :: State
 test1 = readState
   [ "#######"
@@ -75,9 +112,23 @@ test1 = readState
   , "#######"
   ]
 
+test2 :: State
+test2 = readState
+  [ "#######"
+  , "#E..G.#"
+  , "#...#.#"
+  , "#.G.#G#"
+  , "#######"
+  ]
+--
+-- Main
+--
+
 main :: IO ()
 main = do
   input <- readFile "input/day15.txt"
   let initialState = readState $ lines input
-  putStrLn $ "day 12 part a: " ++ "NYI"
-  putStrLn $ "day 12 part b: " ++ "NYI"
+  move True test2 (1,1)
+  return ()
+--  putStrLn $ "day 15 part a: " ++ "NYI"
+--  putStrLn $ "day 15 part b: " ++ "NYI"
