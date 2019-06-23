248c7a4776f98e8f1e1f992c6d057fbb49a01718
Joe Watson <joe_watson@mail.com>
2019-06-23 09:28:38 +0800

    Day 23a completed but not tested

--- src/day23.hs
+++ src/day23.hs
@@ -4,40 +4,71 @@ module Day23 where
 
 import qualified Data.Attoparsec.ByteString.Char8 as AC
 import qualified Data.ByteString as B
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BC
-import Data.ByteString (ByteString)
+import           Data.Foldable (maximumBy)
+import           Data.Ord (comparing)
 
-data Nanobot = Nanobot {
+data Bot = Bot {
   x :: Int
 , y :: Int
 , z :: Int
 , r :: Int
 }
-instance Show Nanobot where
-  show n = "pos=<" ++ show (x n) ++ "," ++ show (y n) ++ "," ++ show (z n) ++ ">, r=" ++ show (r n)
+instance Show Bot where
+  show b = "pos=<" ++ show (x b) ++ "," ++ show (y b) ++ "," ++ show (z b) ++ ">, r=" ++ show (r b)
 
--- | Read a Nanobot record from a line of input
+botDistance :: Bot -> Bot -> Int
+botDistance b1 b2 = abs (x b1 - x b2) + abs (y b1 - y b2) + abs (z b1 - z b2)
+
+-- | Read a Bot record from a line of input
 --
--- >>> readNanobot (BC.pack "pos=<1,1,2>, r=1")
+-- >>> readBot (BC.pack "pos=<1,1,2>, r=1")
 -- pos=<1,1,2>, r=1
-readNanobot :: ByteString -> Nanobot
-readNanobot = either error id . AC.parseOnly nanobot
+readBot :: ByteString -> Bot
+readBot s = either (error . withInput) id . AC.parseOnly bot $ s
   where
-    nanobot :: AC.Parser Nanobot
-    nanobot = do
+    withInput :: String -> String
+    withInput msg = concat [msg, " (reading ", BC.unpack s, ")"]
+    bot :: AC.Parser Bot
+    bot = do
       AC.string "pos=<"
-      x <- AC.decimal
+      x <- AC.signed AC.decimal
       AC.char ','
-      y <- AC.decimal
+      y <- AC.signed AC.decimal
       AC.char ','
-      z <- AC.decimal
+      z <- AC.signed AC.decimal
       AC.string ">, r="
       r <- AC.decimal
-      return Nanobot { x = x, y = y, z = z, r = r}
+      return Bot { x = x, y = y, z = z, r = r}
+
+-- | Solve part A, number of bots in range of the strongest bot
+partA :: [Bot] -> Int
+partA bots = length $ filter (<= r strongestBot) distances
+  where
+    strongestBot :: Bot = maximumBy (comparing r) bots
+    distances :: [Int] = map (botDistance strongestBot) bots
+
+-- | Test for part A from problem statement
+--
+-- >>> partA testA
+-- 7
+testA :: [Bot]
+testA = map readBot
+  [ "pos=<0,0,0>, r=4"
+  , "pos=<1,0,0>, r=1"
+  , "pos=<4,0,0>, r=3"
+  , "pos=<0,2,0>, r=1"
+  , "pos=<0,5,0>, r=3"
+  , "pos=<0,0,3>, r=1"
+  , "pos=<1,1,1>, r=1"
+  , "pos=<1,1,2>, r=1"
+  , "pos=<1,3,1>, r=1"
+  ]
 
 main :: IO ()
 main = do
   input <- B.readFile "input/day23.txt"
-  let bots :: [Nanobot] = map readNanobot $ BC.lines input
-  putStrLn $ "day 23 part a: " ++ show (head bots)
+  let bots :: [Bot] = map readBot $ BC.lines input
+  putStrLn $ "day 23 part a: " ++ show (partA bots)
   putStrLn $ "day 23 part b: " ++ "NYI"
