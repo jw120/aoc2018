45b768422e398464d401e4c9dacf918718ee0f9f
Joe Watson <joe_watson@mail.com>
2019-07-21 23:01:51 +0800

    Added racket day02

--- racket/day02.rkt
+++ racket/day02.rkt
@@ -19,8 +19,28 @@
 (module+ test
   (check-equal? (sort (string->counts "abcaba") <) (list 1 2 3)))
 
+; Part b: Find pair of strings that differ by one character only and return those common characters
+(define (part-b xs)
+  (let ([target-length (- (string-length (first xs)) 1)])
+    (for*/first ([s1 xs]
+                 [s2 xs]
+                 #:when (eq? target-length (string-length (common-characters s1 s2))))
+      (common-characters s1 s2))))
+(module+ test
+  (check-equal? (part-b '("abcde" "fghij" "klmno" "pqrst" "fguij" "axcye" "wvxyz")) "fgij"))
+
+; Return a string with the common characters from the given pair of strings
+(define (common-characters s1 s2)
+  (list->string
+   (for/list ([c1 (in-string s1)]
+              [c2 (in-string s2)]
+              #:when (eq? c1 c2))
+    c1)))
+(module+ test
+  (check-equal? (common-characters "fghij" "fguij") "fgij"))
+
 (define inputs (file->lines "../input/day02.txt"))
 (printf "Day 02a: ~a\n" (part-a inputs))
-(printf "Day 02b: ~a\n" "NYI")
+(printf "Day 02b: ~a\n" (part-b inputs))
 
 
